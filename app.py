3bea50258a202cd633050b028e31a543200ef68b
Richard Cartwright <richard@pchelptoyou.co.uk>
2020-05-18 17:48:33 +0100

    Starting on the models and auth

--- app.py
+++ app.py
@@ -1,27 +1,61 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
+import json
+from models import setup_db, Actor, Movie
+from auth import AuthError, requires_auth
 
-from models import setup_db
 
 def create_app(test_config=None):
-
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
-        return greeting
+    @app.route('/', methods=['GET'])
+    def welcome_page():
+        return 'Nothing to see here!'
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    @app.route('/actors', methods=['GET'])
+    #@requires_auth('get:actors')
+    def get_all_actors():
+        actors = Actor.query.all()
+
+        all_actors = []
+        for actor in actors:
+            all_actors.append(actor.format())
+
+        print(all_actors)
+        return jsonify(all_actors)
+
+    @app.route('/actors', methods=['POST'])
+    def add_new_actor():
+        if request.data:
+            data = request.get_json()
+        else:
+            abort(424)
+
+        # print(data)
+        if not 'name' or not 'age' or not 'gender' in data:
+            print('Something missing')
+            abort(403)
+
+        name = data['name']
+        age = data['age']
+        gender = data['gender']
+
+        actor = Actor(name=name,age=age,gender=gender)
+        actor.insert()
+
+        return jsonify(actor.format())
+
+
+
+
+
 
     return app
+
 
 app = create_app()
 
--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 import os
 
-from sqlalchemy import Column, String, create_engine, Integer
+from sqlalchemy import Column, String, create_engine, Integer, PickleType, Date
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-#database_path = os.environ['DATABASE_URL']
-database_path = 'postgres://cgbzqoqcjifvyh:2c72774b372db538187019e4a9c1c4d32cfcd72437b6441d18fa70036d4b2ec3@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d94ds7k3f5il5p'
+database_path = os.environ['DATABASE_URL']
+# database_path = 'postgres://qzwlkfgdgbtujl:67a8458a8ada7b6c90594c45df6354e9ec910e5b2c2a7df413970a846a1a5fe0@ec2-3-91-139-25.compute-1.amazonaws.com:5432/defl22mrhoku1u'
 
 db = SQLAlchemy()
 
@@ -13,6 +13,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,19 +27,56 @@ def setup_db(app, database_path=database_path):
 Person
 Have title and release year
 '''
-class Person(db.Model):  
-  __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+roles = db.Table('roles',
+                 db.Column('actor_id', db.Integer, db.ForeignKey('actors.id')),
+                 db.Column('movie_id', db.Integer, db.ForeignKey('movies.id')),
+                 )
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(String)
+    gender = Column(String)
+    movies = db.relationship('Movie',
+                             secondary=roles,
+                             backref=db.backref('movies')
+                             )
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def __init__(self, name, catchphrase=""):
+        self.name = name
+        self.catchphrase = catchphrase
+
+    def format(self):
+        return {
+
+        }
